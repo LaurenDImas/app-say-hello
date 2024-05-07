7731e993b6113977aae708c79c6b83073bcba137
LaurenDImas <dimas.yogi.777@gmail.com>
2024-05-07 09:32:44 +0700

    hello

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+// go_say_hello "github.com/LaurenDImas/go-say-hello"
+
+func main() {
+	// fmt.Println(go_say_hello.SayHello())
+}
+
+/*
+	go get github.com/LaurenDImas/go-say-hello
+*/
